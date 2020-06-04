287c571acd51054d608b8569281210cd72ca8b90
Denis FOLLEREAU <denis.follereau06@gmail.com>
2020-06-04 13:15:05 +0200

    Update gateway.py

--- MQTT-broker/gateway.py
+++ MQTT-broker/gateway.py
@@ -8,8 +8,8 @@ import requests
 from datetime import date
 from datetime import datetime
 
-mqqt_server='192.168.1.73' #enter your mqqt server here
-mqqt_port=1883
+mqqt_server = '192.168.1.73' #IP allocated by you or your DHCP/Box for the MQTT gateway
+mqqt_port = 1883
 
 def on_connect(mqttc, obj, flags, rc):
     #print("Connected with result code "+str(rc))
@@ -28,7 +28,7 @@ def on_message(client, userdata, msg):
 
     body='{ "device":"' + str(sample['device']) + '", "sample_date" : "' + time_t.strftime("%Y-%m-%d") + '", "value":"' + str(sample['value']) + '", "time":"' + repr(timestamp_utc) + '" }'
     print("Message: " + body)
-    secret = 'xxxxxx' #b'Enter your MongoDB Stitch Webhook password here'
+    secret = 'xxxxxx' #'MongoDB Stitch Webhook password here'
     hash = hmac.new(bytes(secret, 'latin-1'), body.encode("utf-8"), hashlib.sha256)
     url = 'https://webhooks.mongodb-stitch.com/api/client/v2.0/app/connecteddevices-xmzek/service/iotreceivedata/incoming_webhook/savesensordata' #enter your Stitch Web API URL here
     header={"Content-Type":"application/json","X-Hook-Signature":"sha256=" + hash.hexdigest()  }
